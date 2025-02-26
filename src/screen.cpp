acc6e27a1aacb5707ee6626bb129d70a0d697e02
mato <mato.fefer@gmail.com>
2025-02-26 23:17:24 +0100

    you can draw and stuff

--- src/screen.cpp
+++ src/screen.cpp
@@ -12,10 +12,14 @@ void game_window() {
         return;
     }
 		int win_size_x = 2000, win_size_y = 2000;
+		
+		int x1, x2, y1, y2;
+		bool have_poz1 = false;
+
     window = SDL_CreateWindow("jonny", 100, 100, win_size_x, win_size_y, SDL_WINDOW_SHOWN);    
 		SDL_SetWindowSize(window, win_size_x + 1, win_size_y + 1);
 		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Surface *image = SDL_LoadBMP("./picture/tux.bmp");
+    SDL_Surface *image = SDL_LoadBMP("./picture/tux_the_english.bmp");
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
     SDL_FreeSurface(image);
 		// { x, y, height, width} 
@@ -25,7 +29,7 @@ void game_window() {
     rectangle.w = 40;
     rectangle.h = 40;
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     bool run_game = true;
     while (run_game) {
@@ -39,10 +43,10 @@ void game_window() {
                 if (keystate[SDL_SCANCODE_W]) {
                     std::cout << "w is pressed\n";
 										if(rectangle.y > 0){
-												rectangle.y-=5;
 												SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-												SDL_RenderClear(renderer);
+												SDL_RenderFillRect(renderer, &rectangle); // the problem with render clear is that it clears the whole screen
 												SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+												rectangle.y-=5;
 										}
 										else{
 												std::cout << "there is too little space so move other direction\n";
@@ -51,10 +55,10 @@ void game_window() {
                 if (keystate[SDL_SCANCODE_S]) {
                     std::cout << "s is pressed\n";
 										if(rectangle.y < win_size_y){
-												rectangle.y+=5;
 												SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-												SDL_RenderClear(renderer);
+												SDL_RenderFillRect(renderer, &rectangle);
 												SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+												rectangle.y+=5;
 										}
 										else{
 												std::cout << "there is too little space so move other direction\n";
@@ -63,10 +67,10 @@ void game_window() {
                 if (keystate[SDL_SCANCODE_A]) {
                     std::cout << "a is pressed\n";
 										if(rectangle.x > 0){
-												rectangle.x-=5;
 												SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-												SDL_RenderClear(renderer);
+												SDL_RenderFillRect(renderer, &rectangle);
 												SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+												rectangle.x-=5;
 										}
 										else{
 												std::cout << "there is too little space so move other direction\n";
@@ -75,13 +79,26 @@ void game_window() {
                 if (keystate[SDL_SCANCODE_D]) {
                     std::cout << "d is pressed\n";
 										if(rectangle.x < win_size_x-20){
-												rectangle.x+=5;
 												SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-												SDL_RenderClear(renderer);
+												SDL_RenderFillRect(renderer, &rectangle);
 												SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+												rectangle.x+=5;
 										}
 										else{
 												std::cout << "there is too little space so move other direction\n";
+										}
+								}
+								if (keystate[SDL_SCANCODE_X]) {
+										// i want to draw a line
+										if(!have_poz1){
+												SDL_GetMouseState(&x1, &y1);
+												have_poz1 = !have_poz1;
+										}
+										else{
+												// draw
+												SDL_GetMouseState(&x2, &y2);
+												SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+												have_poz1 = !have_poz1;
 										}
 								}
             }
