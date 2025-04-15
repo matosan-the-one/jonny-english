e89566123089b55801035b63bbe79138da1b40eb
matosan-the-one <mato.fefer3@gmail.com>
2025-04-15 13:58:40 +0200

    i made one map yay

--- src/files.cpp
+++ src/files.cpp
@@ -3,10 +3,11 @@
 #include "ball.h"
 
 void maps(std::vector <Line> buf, int n, int si){
-	std::ofstream data("./file_dump/walls");	
+	std::ofstream data("./file_dump/walls.txt");	
 	for(auto const &x : buf){
-		data << x.x1 << ","<< x.y1 << "," << x.x2 << "," << x.y2 << "\n";  	
+		data << x.x1 << " "<< x.y1 << " " << x.x2 << " " << x.y2 << "\n";
 	}
 	data.close();
 }
+
 
