809a37c89e93bfc0f4f5bf76eac5721eada0e1ee
matosan-the-one <mato.fefer3@gmail.com>
2025-04-22 14:20:07 +0200

    complete replay

--- src/replay_m.cpp
+++ src/replay_m.cpp
@@ -1,4 +1,7 @@
 #include <fstream>
+#include <vector>
+#include <SDL2/SDL.h>
+#include "ball.h"
 
 void replay(int x, int y) {
     static int g , l;
@@ -21,32 +24,81 @@ void replay_save(int i){
 }
 
 void give_me_replay() {
-    ifstream data0 ("./player_log/replay/replay1.txt");
-    ifstream data1 ("file_dump/map1.txt");
-       
+    std::ifstream data0 ("./player_log/replay/replay1.txt");
+    std::ifstream data1 ("file_dump/map1.txt");
+    std::vector<Line> tab_l ;
+    std::vector<int> x_p, y_p;
+    int len1, len2, len3;
+    int ux, uy;
+    while(data0>>ux>>uy){
+        x_p.push_back(ux);
+        y_p.push_back(uy);
+    }
+    len1=x_p.size(); 
+    data0.close();
+    data0.open("./player_log/replay/replay2.txt");
+    while(data0>>ux>>uy){
+        x_p.push_back(ux);
+        y_p.push_back(uy);
+    }
+    len2=x_p.size()-len1;
+    data0.close();
+    data0.open("./player_log/replay/replay3.txt");
+    while(data0>>ux>>uy){
+        x_p.push_back(ux);
+        y_p.push_back(uy);
+    }
+    data0.close(); 
+    len3=x_p.size()-len2;
+
     SDL_Window *window = nullptr;
-           if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-                   return -1;
-           }
-           int win_size_x = 1920, win_size_y = 1080;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+       return ;
+    }
+    int win_size_x = 1920, win_size_y = 1080;
     window = SDL_CreateWindow("jonny", 100, 100, win_size_x, win_size_y, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    bool run=1;
 
-SDL_Rect player;
-           player.x = 50;
-           player.y = 100;
-           player.w = 20;
-           player.h = 20;
-
-SDL_RenderFillRect( renderer, &player );
-
-               SDL_RenderPresent(renderer);
-               SDL_Delay(10);
-
-
-               for(auto &line : tab_l){
-                 line.draw(renderer);
-               }
+    int count=0, i=0;
+    SDL_Rect player;
+    player.x = 50;
+    player.y = 50; 
+    player.w = 20;
+    player.h = 20;
+    while(run){
+        SDL_Event event;
+        while (SDL_PollEvent(&event)){
+             if (event.type == SDL_QUIT){
+                 run = 0;
+             }
+        }
+        if(i==len1){
+            data1.open ("file_dump/map2.txt");
+        }
+        if(i==len1+len2){
+           int x1, x2, y1, y2; 
+            data1.open ("file_dump/map3.txt");
+            while(data1>>){
+                data1.emplace_back()
+            }
+        }
+        if(i==len1+len2+len3)
+            run=0;
+        if(count==20){
+            count=0;
+            i++;
+            player.x=ux[i];
+            player.y=uy[i];
+        }
+        SDL_RenderFillRect(renderer, &player);
+        for(auto &line : tab_l){
+            line.draw(renderer);
+        }
+        SDL_RenderPresent(renderer);
+        SDL_Delay(10);
+        SDL_RenderClear(renderer);
+    }
 
 
 }
